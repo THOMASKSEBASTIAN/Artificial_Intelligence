bafbb69a66c8e15378706c96a6bd94ec50b527b8
Thomas K Sebastian <thomasks1994@gmail.com>
2022-10-07 09:38:24 +0530

    first commit

--- Python/Exercises/Practice.py
+++ Python/Exercises/Practice.py
@@ -14,7 +14,12 @@ for i in range(10):
 
 
 
-# Exercise 3: Print characters from a string that are present at an even index number
+
+
+
+
+
+# Exercise 2: Print characters from a string that are present at an even index number
 
 Name=input("Enter the Name")
 for i in Name:
@@ -24,3 +29,49 @@ for i in Name:
         print(i)
 
 
+# Exercise 3: Remove first n characters from a string
+
+name=input("Enter the name")
+number=int(input("Enter the number of element"))
+name2=[]
+for i in name:
+    if name.find(i)>=number:
+        name2.append(i)
+listToStr = ' '.join([str(elem) for elem in name2])
+print(listToStr)
+
+
+# Exercise 4: Addition of natural numbers
+number=int(input("Enter the number of element"))
+sum=0
+for i in range(number+1):
+    print(i)
+    sum=sum+i
+print(sum)
+
+
+# Exercise 5: factorials
+number=int(input("Enter the number of element"))
+factorial=1
+for i in range(1,number+1):
+    print(i)
+    factorial=factorial*i
+print(factorial)
+
+
+# Exercise 6: While loop
+num=1
+while num<=10:
+    print(num)
+    num=num+1
+#
+
+
+# Functions
+# Functions are of 3 types
+# 1. Function without arguments
+def add():
+    number1=int(input("Enter the number of element"))
+    number2 = int(input("Enter the number of element"))
+    print(number1+number2)
+add()
